f62381e7684e5559434615db68015a78504eb27c
Matthew Lyons <matthew@nowaffles.com>
2021-12-14 10:39:40 -0800

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -21,8 +21,9 @@ class MkxpConan(ConanFile):
     exports_sources = "*"
     requires = (
         "boost/1.77.0",
-        "openal/1.18.2@bincrafters/stable",
-        "physfs/3.0.1@astrabit/testing",
+        #"openal/1.18.2@bincrafters/stable",
+        "openal/1.21.1",
+	"physfs/3.0.1@astrabit/testing",
         "pixman/0.34.0@astrabit/testing",
         "ruby/3.0.2@astrabit/testing",
         "sdl2_image/2.0.5@bincrafters/stable",
