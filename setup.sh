61089257b51108be65875535e38c08eeb05c1c26
Speak2Erase <matthew@nowaffles.com>
2022-02-10 15:08:55 -0800

    I swear if this is what the problem was

--- setup.sh
+++ setup.sh
@@ -202,8 +202,8 @@ git clone $sdl2_url $sdl2_path
 cd $sdl2_path
 mkdir build
 cd build
-../configure --prefix="$libpath/sdl2_dest"
-export SDL_LIBS="$libpath/sdl2_dest/lib"
+../configure --prefix="$libpath/sdl2_dest/"
+export SDL_LIBS="$libpath/sdl2_dest/lib/"
 export SDL2_CONFIG="$libpath/sdl2_dest/bin/sdl2-config"
 export SDL2_INCLUDE_DIR="$libpath/sdl2_dest/include"
 PATH="$PATH:$libpath/sdl2_dest/bin"
