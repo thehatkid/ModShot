df5945b5b1fec548e599a0f1b36dbf51a64323a0
somedevfox <somedevfox@gmail.com>
2022-03-31 21:57:52 +0300

    Fix broken dependancy.

--- conanfile.py
+++ conanfile.py
@@ -27,7 +27,7 @@ class MkxpConan(ConanFile):
         "ruby/3.1.1@astrabit/testing",
         "sdl2_image/2.0.5@bincrafters/stable",
         "sdl2_ttf/2.0.15@bincrafters/stable",
-        "sdl_sound/1.0.1@astrabit/testing",
+        "sdl_sound-mkxp/1.0.1@eliza/stable",
         "sigc++/2.10.0@astrabit/testing",
         # Overrides
         "libpng/1.6.37",
